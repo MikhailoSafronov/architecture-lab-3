af18b2a385eabc43d53ae0fab0cca4c406e56e75
SVersj <86892022+SVersj@users.noreply.github.com>
2025-04-18 23:38:00 +0300

    Update painter_test.go

--- painter_test.go
+++ painter_test.go
@@ -3,12 +3,12 @@ package painter
 import "testing"
 
 func TestTypesExist(t *testing.T) {
-
-    var _ Texture
-    var _ OperationFunc
+    // Просто проверяем что типы доступны
+    var _ Texture = nil
+    var _ OperationFunc = nil
     var _ = BgRect{}
     var _ = Move{}
-    var _ = TFigure{}
+    var _ = TFigure{} 
     var _ = WhiteFill{}
     var _ = GreenFill{}
     var _ = UpdateOp{}
