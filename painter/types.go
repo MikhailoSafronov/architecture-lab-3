cab2ea844712af521781eee6ac73f8a408d84d0a
SVersj <86892022+SVersj@users.noreply.github.com>
2025-04-18 23:22:02 +0300

    Update types.go

--- painter/types.go
+++ painter/types.go
@@ -1,32 +1,22 @@
 package painter
 
-// Texture представляет текстуру для рисования
 type Texture interface{}
 
-// OperationFunc - тип для операций рисования
 type OperationFunc func(Texture)
 
-// BgRect - прямоугольник фона
 type BgRect struct {
     X1, Y1, X2, Y2 float32
 }
 
-// Move - операция перемещения
 type Move struct {
     X, Y float32
 }
 
-// TFigure - фигура типа T
 type TFigure struct {
     X, Y float32
-    Angle int // Угол поворота для T-фигуры
+    Angle int
 }
 
-// WhiteFill - операция заливки белым
 type WhiteFill struct{}
-
-// GreenFill - операция заливки зеленым
 type GreenFill struct{}
-
-// UpdateOp - операция обновления экрана
-type UpdateOp struct{}
+type UpdateOp struct{} // Перенесено из op.go
