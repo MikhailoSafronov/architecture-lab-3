2f238f3104ea01d59b7086a1d1902f9fa2195fc4
SVersj <86892022+SVersj@users.noreply.github.com>
2025-04-18 23:15:05 +0300

    Update operation.go

--- painter/operation.go
+++ painter/operation.go
@@ -1,11 +1,10 @@
 package painter
 
+type Texture interface{}
+
 type OperationFunc func(Texture)
 
-type BgRect struct {
-    X1, Y1, X2, Y2 float32
-}
-
-type Move struct {
-    X, Y float32
-}
+type BgRect struct { X1, Y1, X2, Y2 float32 }
+type Move struct { X, Y float32 }
+type TFigure struct { X, Y float32 }
+type WhiteFill struct{}
