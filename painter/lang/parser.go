813937822595e8ca9c8d05f6490b154fc490f68f
SVersj <86892022+SVersj@users.noreply.github.com>
2025-04-19 16:35:52 +0300

    Update parser.go

--- painter/lang/parser.go
+++ painter/lang/parser.go
@@ -1,6 +1,9 @@
 package lang
 
 import (
+    "fmt"
+    "io"
+    "io/ioutil"
     "strings"
     "strconv"
     "github.com/roman-mazur/architecture-lab-3/painter"
@@ -8,8 +11,18 @@ import (
 
 type Parser struct{}
 
-func (p *Parser) Parse(cmd string) ([]painter.Operation, error) {
+func (p *Parser) Parse(r io.Reader) ([]painter.Operation, error) {
+    data, err := ioutil.ReadAll(r)
+    if err != nil {
+        return nil, err
+    }
+    
+    cmd := string(data)
     parts := strings.Fields(cmd)
+    if len(parts) == 0 {
+        return nil, fmt.Errorf("empty command")
+    }
+    
     switch parts[0] {
     case "bgrect":
         return p.parseBgRect(parts[1:])
@@ -29,14 +42,53 @@ func (p *Parser) parseBgRect(args []string) ([]painter.Operation, error) {
         return nil, fmt.Errorf("bgrect requires 4 arguments")
     }
     
-    coords := make([]float32, 4)
+    coords := make([]float64, 4)
     for i, arg := range args {
-        val, _ := strconv.ParseFloat(arg, 32)
-        coords[i] = float32(val)
+        val, err := strconv.ParseFloat(arg, 64)
+        if err != nil {
+            return nil, fmt.Errorf("invalid coordinate %s: %v", arg, err)
+        }
+        coords[i] = val
     }
     
-    return []painter.Operation{painter.BgRect{
+    return []painter.Operation{painter.BgRectOp{
         X1: coords[0], Y1: coords[1],
         X2: coords[2], Y2: coords[3],
     }}, nil
+}
+
+func (p *Parser) parseFigure(args []string) ([]painter.Operation, error) {
+    if len(args) != 2 {
+        return nil, fmt.Errorf("figure requires 2 arguments")
+    }
+    
+    x, err := strconv.ParseFloat(args[0], 64)
+    if err != nil {
+        return nil, fmt.Errorf("invalid x coordinate: %v", err)
+    }
+    
+    y, err := strconv.ParseFloat(args[1], 64)
+    if err != nil {
+        return nil, fmt.Errorf("invalid y coordinate: %v", err)
+    }
+    
+    return []painter.Operation{painter.FigureOp{X: x, Y: y}}, nil
+}
+
+func (p *Parser) parseMove(args []string) ([]painter.Operation, error) {
+    if len(args) != 2 {
+        return nil, fmt.Errorf("move requires 2 arguments")
+    }
+    
+    x, err := strconv.ParseFloat(args[0], 64)
+    if err != nil {
+        return nil, fmt.Errorf("invalid x coordinate: %v", err)
+    }
+    
+    y, err := strconv.ParseFloat(args[1], 64)
+    if err != nil {
+        return nil, fmt.Errorf("invalid y coordinate: %v", err)
+    }
+    
+    return []painter.Operation{painter.MoveOp{X: x, Y: y}}, nil
 }
