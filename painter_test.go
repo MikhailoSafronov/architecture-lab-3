eebdc73e08867830543edc33821bb42ee228ffc9
SVersj <86892022+SVersj@users.noreply.github.com>
2025-04-19 13:11:40 +0300

    Update painter_test.go

--- painter_test.go
+++ painter_test.go
@@ -1,15 +1,13 @@
-package painter
+package painter_test
 
-import "testing"
+import (
+    "testing"
+    "github.com/roman-mazur/architecture-lab-3/painter"
+    "github.com/stretchr/testify/assert"
+)
 
-func TestTypesExist(t *testing.T) {
-    // Просто проверяем что типы доступны
-    var _ Texture = nil
-    var _ OperationFunc = nil
-    var _ = BgRect{}
-    var _ = Move{}
-    var _ = TFigure{} 
-    var _ = WhiteFill{}
-    var _ = GreenFill{}
-    var _ = UpdateOp{}
+func TestBgRectOperation(t *testing.T) {
+    op := painter.BgRect{X1: 0.1, Y1: 0.2, X2: 0.3, Y2: 0.4}
+    assert.Equal(t, float32(0.1), op.X1)
+    // Добавьте проверки выполнения операции
 }
