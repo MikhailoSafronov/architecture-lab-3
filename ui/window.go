934f8ff4a1b52a0257400f52bbac13532ecd5919
SVersj <86892022+SVersj@users.noreply.github.com>
2025-04-19 16:41:51 +0300

    Update window.go

--- ui/window.go
+++ ui/window.go
@@ -39,7 +39,12 @@ func (pw *Visualizer) Main() {
 func (pw *Visualizer) Update(t screen.Texture) {
 	pw.tx <- t
 }
-
+func NewWindow() (*Visualizer, error) {
+    return &Visualizer{
+        Title: "Painter App",
+        Debug: false,
+    }, nil
+}
 func (pw *Visualizer) run(s screen.Screen) {
 	w, err := s.NewWindow(&screen.NewWindowOptions{
 		Title:  pw.Title,
