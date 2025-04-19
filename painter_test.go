3195da0dda8fb5dcc6a1d2e9f80ad5bd52efecd0
SVersj <86892022+SVersj@users.noreply.github.com>
2025-04-19 14:09:24 +0300

    Update painter_test.go

--- painter_test.go
+++ painter_test.go
@@ -2,12 +2,12 @@ package painter_test
 
 import (
     "testing"
-    "github.com/MikhailoSafronov/architecture-lab-3/painter"
+    "github.com/roman-mazur/architecture-lab-3/painter"
     "github.com/stretchr/testify/assert"
 )
 
 func TestBgRectOperation(t *testing.T) {
     op := painter.BgRect{X1: 0.1, Y1: 0.2, X2: 0.3, Y2: 0.4}
     assert.Equal(t, float32(0.1), op.X1)
-    // Добавьте проверки выполнения операции
+    
 }
