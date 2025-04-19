4ecbba74434c6ea9bf67d18d22ec8467c25fd650
SVersj <86892022+SVersj@users.noreply.github.com>
2025-04-19 16:34:15 +0300

    Update types.go

--- painter/types.go
+++ painter/types.go
@@ -1,11 +1,5 @@
 package painter
 
-// Все типы в одном файле без дублирований
-type Texture interface{}
-type OperationFunc func(Texture)
-
-type BgRect struct{}
-type Move struct{} 
-type TFigure struct{}
+type AlternativeTexture interface{}
 type WhiteFill struct{}
 type GreenFill struct{}
